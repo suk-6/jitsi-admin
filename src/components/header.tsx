3763e953a25b0902c36ad8a5733bf8e2109a8fb7
suk-6 <me@suk.kr>
2024-09-02 17:06:09 +0900

    refactor: Update header text in Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
 	return (
 		<div className=" w-full h-12 flex justify-between bg-header-background px-4 py-1">
 			<div className=" flex items-center">
-				<div className="text-xl font-semibold">Jitsi Admin</div>
+				<div className="text-xl font-semibold">Jitsi</div>
 			</div>
 		</div>
 	);
