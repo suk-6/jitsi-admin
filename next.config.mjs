9516707e9a29f4c219a77bcfe76019a57a220441
suk-6 <me@suk.kr>
2024-09-02 15:45:52 +0900

    feat: add remotePatterns for images in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'file.suk.kr',
+        port: '',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
