3852dea2ece8b29c0304f44483682b0e1024f189
suk-6 <me@suk.kr>
2024-09-02 15:46:17 +0900

    chore: Add header component to the project

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+	return (
+		<div className=" w-full h-12 flex justify-between bg-header-background px-4 py-1">
+			<div className=" flex items-center">
+				<div className="text-xl font-semibold">Jitsi Admin</div>
+			</div>
+		</div>
+	);
+};
